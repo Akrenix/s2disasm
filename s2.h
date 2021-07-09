8cc768a9b152c0529f7537e36704cef26f06a940
Akrenix <30519221+Akrenix@users.noreply.github.com>
2021-07-09 13:09:39 -0700

    Add files via upload

---
+++ s2.h
@@ -0,0 +1,7 @@
+comp_z80_size 0xF4A le for C Program */
+#define word_728C_user 0x7178
+#define Obj5F_MapUnc_7240 0x71F8
+#define off_3A294 0x3A0FA
+#define MapRUnc_Sonic 0x71340
+#define movewZ80CompSize 0xEC04E
+/* Ende Include File for C Program */
